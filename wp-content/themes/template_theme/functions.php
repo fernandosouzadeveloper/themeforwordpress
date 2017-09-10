3df1b26fb239a45c7a93533830f27983b9a10839
fernandosouzadeveloper <fernandosouzawebdesign>
2017-09-10 14:43:13 -0300

    mudanças na navegação do tema

--- wp-content/themes/template_theme/functions.php
+++ wp-content/themes/template_theme/functions.php
@@ -20,6 +20,19 @@
 
 // add_filter('show_admin_bar', '__return_false');
 
+add_theme_support( 'menus');
+
+	function register_theme_menus(){
+	register_nav_menus(
+		array(
+			'header-menu' => __('Header Menu')
+			)
+		);
+
+}
+add_action( 'init', 'register_theme_menus');
+
+
 
 
 ?>
